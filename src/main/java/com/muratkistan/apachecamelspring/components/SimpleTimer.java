c97d6fb6ef02cb5c34d4032ae813467cb8829d42
muratkistan <muratkistan@gmail.com>
2022-06-10 00:35:34 +0300

    Simple Timer Route

---
+++ src/main/java/com/muratkistan/apachecamelspring/components/SimpleTimer.java
@@ -0,0 +1,17 @@
+package com.muratkistan.apachecamelspring.components;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleTimer extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        from("timer:simpletimer?period=2000")
+                .routeId("simpleTimerId")
+                .setBody(constant("Hello World"))
+                .log(LoggingLevel.INFO,"${body}");
+    }
+}
