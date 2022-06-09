df53dbe168559b624e32f5e6700e4f01942b2074
muratkistan <muratkistan@gmail.com>
2022-06-10 00:17:34 +0300

    First Commit

---
+++ src/main/java/com/muratkistan/apachecamelspring/ApacheCamelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.muratkistan.apachecamelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muratkistan/apachecamelspring/ApacheCamelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratkistan.apachecamelspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
