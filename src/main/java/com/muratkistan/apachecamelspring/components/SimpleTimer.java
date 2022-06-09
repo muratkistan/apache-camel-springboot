d2bb8c89c56d3ae66956c7feff94e6266584d52f
muratkistan <muratkistan@gmail.com>
2022-06-10 01:08:56 +0300

    Edit @Component annotation

--- src/main/java/com/muratkistan/apachecamelspring/components/SimpleTimer.java
+++ src/main/java/com/muratkistan/apachecamelspring/components/SimpleTimer.java
@@ -4,7 +4,7 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SimpleTimer extends RouteBuilder {
     @Override
     public void configure() throws Exception {
