8a2152dbdab3406e15ae6005a60c384481486db6
muratkistan <muratkistan@gmail.com>
2022-06-15 15:31:37 +0300

    Added rest() for saving database

--- src/main/java/com/muratkistan/apachecamelspring/route/NewRestRoute.java
+++ src/main/java/com/muratkistan/apachecamelspring/route/NewRestRoute.java
@@ -1,6 +1,7 @@
 package com.muratkistan.apachecamelspring.route;
 
 import com.muratkistan.apachecamelspring.beans.NameAddress;
+import com.muratkistan.apachecamelspring.processor.InboundMessageProcessor;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
@@ -18,11 +19,25 @@ public class NewRestRoute extends RouteBuilder {
                 .bindingMode(RestBindingMode.json)
                 .enableCORS(true);
 
-        rest("masterclass")
+//        rest("/masterclass")
+//                .produces("application/json")
+//                .post("/nameAddress").type(NameAddress.class).route()
+//                .routeId("newRestRouteId")
+//                .log(LoggingLevel.INFO,"${body}")
+//                .process(new InboundMessageProcessor())
+//                .log(LoggingLevel.INFO," Transformed Body : ${body}")
+//                .convertBodyTo(String.class)
+//                .to("file:src/data/output?fileName=outputFile.csv&fileExist=append&AppendChars=\\n");
+
+        rest("/masterclass")
                 .produces("application/json")
-                .post("nameAddress").type(NameAddress.class).route()
+                .post("/nameAddress").type(NameAddress.class).route()
                 .routeId("newRestRouteId")
                 .log(LoggingLevel.INFO,"${body}")
-                .process();
+//                .process(new InboundMessageProcessor())
+//                .log(LoggingLevel.INFO," Transformed Body : ${body}")
+//                .convertBodyTo(String.class)
+//                .to("file:src/data/output?fileName=outputFile.csv&fileExist=append&AppendChars=\\n");
+                .to("jpa:"+NameAddress.class.getName());
     }
 }
