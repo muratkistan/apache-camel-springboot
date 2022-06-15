7af60ba0f867e295b5ef6a770b6cb3e9cf5c01ae
muratkistan <muratkistan@gmail.com>
2022-06-15 15:29:50 +0300

    Created Entity classes

--- src/main/java/com/muratkistan/apachecamelspring/beans/NameAddress.java
+++ src/main/java/com/muratkistan/apachecamelspring/beans/NameAddress.java
@@ -2,12 +2,21 @@ package com.muratkistan.apachecamelspring.beans;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "NAME_ADDRESS")
 public class NameAddress {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
+    @Column(name = "house_number")
     private String houseNumber;
     private String city;
     private String province;
+    @Column(name = "postal_code")
     private String postalCode;
 }
